6ab5a57192c0b7575fbd15e0d5b3643126bb8dc7
Moritz Moxter <moritz@clockworks.io>
2023-01-12 10:11:37 +0100

    feat: implements pod optimizations of #7
    This commit updates how the pod is build. This includes
    - writing a correct Pom file according to babashkas pod spec
    - including different platforms and architectures for pods
    - versioned file names for pods
    - cleaning everything up
    - fix a minor parser error
    - add one additional test

--- src/clj/user.clj
+++ src/clj/user.clj
@@ -1,33 +1,50 @@
 (ns user)
 
-(use 'clci.conventional-commit :reload)
 
-(def commit-msg (slurp ".git/COMMIT_EDITMSG"))
+(comment 
+  (use 'clci.conventional-commit :reload)
 
-(require '[instaparse.core :as insta])
+  (def commit-msg (slurp ".git/COMMIT_EDITMSG"))
 
-(-> (insta/parser grammar) (insta/parse commit-msg))
+  (require '[instaparse.core :as insta])
+
+  (-> (insta/parser grammar) (insta/parse commit-msg))
 
 
-(def msg 
-(str
-      "feat: switch to new API #RR-33\n\n"
-      "BREAKING CHANGE: With this commit we are switching to the new API. Please update your access token!\n\n"
+  (def msg 
+    (str
+      "feat: implements pod optimizations of #7\n"
+      "\n"
+      "This commit updates how the pod is build. This includes\n"
+      "- writing a correct Pom file according to babashkas pod spec\n"
+      "- including different platforms and architectures for pods\n"
+      "- versioned file names for pods\n"
+      "- cleaning everything up\n"
       "# Please enter the commit message for your changes. Lines starting\n"
-      "#  with '#' will be ignored, and an empty message aborts the commit.\n"
+      "# with '#' will be ignored, and an empty message aborts the commit.\n"
       "#\n"
-      "# Date:      Thu Dec 8 17:01:23 2022 +0100\n"
-      "# On branch feat/rr-2\n"
+      "# On branch feat/clci-7\n"
       "# Changes to be committed:\n"
-      "#       renamed:    ci-bb/src/clj/ci_bb/pod.clj -> ci-bb/src/clj/ci_bb/main.clj")
+      "# new file:   .babashka/clci-0.1.2.main.metadata.cache\n"
+      "# modified:   bb.edn\n"
+      "# modified:   bb/build.clj\n"
+      "# modified:   build.clj\n"
+      "# modified:   deps.edn\n"
+      "# deleted:    manifest.edn\n"
+      "# new file:   resources/clockworksio/clci/pod-manifest.edn\n"
+      "# modified:   src/clj/user.clj\n"
+      "# modified:   test/clci/conventional_commit_test.clj\n"
+      "#\n")
+    )
+
+
+
+  (-> (insta/parser grammar) (insta/parse msg {:trace true :total true}))
+
+  (-> (insta/parser grammar) (insta/parses msg))
+
+  (-> (insta/parser grammar) (insta/parse msg {:trace true}) (insta/get-failure))
+
+  ;(= (-> (insta/parser grammar) (insta/parse msg)) result)
+
   )
-
-
-
-(-> (insta/parser grammar) (insta/parse msg {:trace true :total true}))
-
-(-> (insta/parser grammar) (insta/parses msg))
-
-(-> (insta/parser grammar) (insta/parse msg {:trace true}) (insta/get-failure))
-
-;(= (-> (insta/parser grammar) (insta/parse msg)) result)
