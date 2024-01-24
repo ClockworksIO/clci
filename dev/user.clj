96fee8df2748e6c921ec30b20ae35f26fe6ec866
Moritz Moxter <moritz@clockworks.io>
2024-01-24 15:21:21 +0100

    chore: add stubbs for dialogs

--- dev/user.clj
+++ dev/user.clj
@@ -1,154 +1,175 @@
 (ns user
   ""
   (:require
+    [clci.util.core :refer [in?]]
     [clci.conventional-commit :as cc]
     [clci.git :as git]
     [clci.release :as rel]
-    [clci.repo :as rp]
+    [clci.repo :as repo]
+    [clci.term :refer [blue red green yellow grey white cyan]]
     [clojure.core :as c]
-    [clojure.string :as str]))
-
-
-(require '[babashka.fs :as fs])
-
-(require '[clojure.string :as str])
-
-
-;; (require '[instaparse.core :as insta])
-
-;; (require '[clojure.walk :refer [walk prewalk postwalk]])
-
-(require '[clci.changelog :refer [text->ast changelog-exists? create-changelog-stub
-                                  changelog-ast-add-release
-                                  changelog-ast-add-unreleased
-                                  ast->text]])
-
-
-(require '[clci.util.core :refer [any mapv-r find-first join-paths]])
-
-
-;; (require '[clci.util.tree :refer [ast-contains-node? ast-get-node ast-dissoc-nodes ast-insert-after]])
-
-
-(require '[clci.release :as rel])
-(require '[clci.git :as git])
-
-(require '[clci.repo :as rp])
-(require '[clci.conventional-commit :as cc])
-
-
-(def example-ast
-  "An example AST."
-  [:AST
-   [:PREAMBLE
-    [:PARAGRAPH
-     [:TEXT "All notable changes to this project will be documented in this file."]]
-    [:PARAGRAPH
-     [:TEXT "The format is loosely based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/) and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html)."]]]
-   [:UNRELEASED
-    [:OTHER
-     [:SHORT-DESC
-      [:TEXT "update build pipeline for releases "]
-      [:ISSUE-REF [:ISSUE-ID "11"]]]]]
-   [:RELEASE
-    [:RELEASE-HEAD
-     [:RELEASE-TAG "0.17.0"]
-     [:DATE "2023-04-14"]]
-    [:ADDED
-     [:LONG-DESC
-      [:PARAGRAPH
-       [:TEXT "new git hook mechanism "]
-       [:ISSUE-REF [:ISSUE-ID "96"]]]
-      [:PARAGRAPH
-       [:TEXT "This commit adds a new mechanism how to use git hooks in projects using clci. A hook will send a trigger which is then processed using the clci workflow mechanism."]]
-      [:PARAGRAPH
-       [:TEXT "And one more line with a linebreak."]
-       [:TEXT "So now what?"]]]]]])
+    [clojure.string :as str]
+    [clojure.spec.alpha :as s]
+    [babashka.process :refer [sh shell]]
+    [babashka.fs :as fs]
+    [bblgum.core :refer [gum]]))
 
 
 
-(def scm-owner "clockworksIO")
-(def scm-repo        "clci")
+(defn installed?
+  "Test if gum is installed on the machine."
+  []
+  (try 
+    (shell {:out :string :err :string} "gum --version")
+    true
+    (catch Exception e
+     false
+      )))
 
 
-;; (def repo
-;;   {:scm {:provider {:name :github
-;;                     :owner scm-owner
-;;                     :repo scm-repo}}})
+(defn- shell-exit-with-error?
+  "Test if the given `exit-code` indicates a shell process did exit with an error."
+  [exit-code]
+  (not= exit-code 0))
 
-(def repo (rp/read-repo))
+(defn- yield-failure
+  "Yield an error map for a dialog `step` when a `failure?` occured."
+  [failure? step]
+  {:step      step
+   :failure?  failure?})
 
+(defn- start-in-new-line
+  "Begin terminal output on a new line."
+  []
+  (print "\n"))
 
-(def latest-release
-  (rel/get-latest-release repo))
-
-
-(def commits-since-release
-  (git/commits-on-branch-since {:since (get-in latest-release [:commit :hash]) :branch "feat/clci-94"}))
-
-
-(def amended-commits-since-release
-  (rel/amend-commit-log commits-since-release))
-
-
-amended-commits-since-release
-
-
-(defn update-changelog!-impl
-  "Update the changelog.
-   Takes at least the `commit-log`.
-   The optional `release` is a map conform to the `:clci.release/release` spec.
-   If no release is provided, then all changes described in the commit-log will
-   be added to the Unreleased section of the changelog. Otherwise a new Release
-   is added to the changelog and all pending changes in the Unreleased section
-   are erased.
-   If no changelog exists an empty changelog will be created and filled with the
-   new entries derived from the commit-log.
-   Writes to the `changelog.md` file!"
-  ;; ([commit-log product] (update-changelog!-impl commit-log product nil))
-  ([commit-log product release]
-   (when-not (changelog-exists?)
-     (create-changelog-stub))
-   (let [changelog-path          (join-paths (:root product) "CHANGELOG.md")
-         current-changelog-text  (slurp changelog-path)
-         changelog-ast (text->ast current-changelog-text)
-         changelog-ast  (if (some? release)
-                          (changelog-ast-add-release changelog-ast commit-log (:tag release) (:published release))
-                          (changelog-ast-add-unreleased changelog-ast commit-log))]
-     (spit changelog-path
-           (ast->text changelog-ast))
-     changelog-ast)))
+(defn- print-welcome
+  "Print the clci assistant welcome message on the terminal."
+  []
+  (print "Welcome to the" (yellow "clci") "setup assistant.\n"))
 
 
-(defn update-changelog!
+(defmulti render (fn [{:keys [element]} _] element))
+
+
+(defmethod render :text [elem history]
+  (if (:linebreak? elem)
+    (print (str (:text elem) "\n"))
+    (print (:text elem)))
+  {:step (:step elem)}
+ )
+
+(defmethod render :choose [elem history]
+  (let [filter-fn               (get elem :filter #(fn [_ options] options))
+        options                 (filter-fn history (:options elem))
+        limit                   (get elem :limit 1)
+        step                    (:step elem)
+        {:keys [status result]} (gum :choose (mapv :name options) :limit limit)]
+    (if (= status 0)
+      {:step step
+       :selected-options (filter (fn [opt] (in? result (:name opt))) options)}
+      {:step      step
+       :failure?  true})))
+
+;(gum :choose ["A" "B"] :limit 1)
+
+
+(defmethod render :wait [elem history]
+ (-> (shell {:out :string} (format "sleep %s" (get elem :seconds 0))) 
+   :exit 
+   (shell-exit-with-error?)
+   (yield-failure (:step elem))
+   )
+ )
+
+
+(defn run-linear-dialog
   ""
-  ([commit-log] (update-changelog! commit-log nil))
-  ([commit-log release]
-   (let [all-products  (rp/get-products)
-         products-map  (->> commit-log
-                            (map
-                              (fn [commit]
-                                (into (hash-map)
-                                      (map
-                                        (fn [product] [product (list commit)])
-                                        (git/affected-products commit all-products)))))
-                            (apply (partial merge-with into)))]
-     (doseq [[product-key commits] products-map]
-       (update-changelog!-impl commits (find-first (fn [p] (= (:key p) product-key)) all-products) release)))))
+  [dialog]
+  (start-in-new-line)
+  (print-welcome)
+  (loop [head     (first dialog)
+         tail     (rest dialog)
+         history  []]
+      (cond 
+        ;; no more items left
+        (nil? head)
+        history
+        ;; error on the previous item
+        (-> history last :failure?)
+        (ex-info "Unable to execute full dialog!" {:history history})
+        ;; otherwise continue with the next element
+        :else
+        (recur (first tail) (rest tail) (conj history (render head history)))
+        )
+    )
+  )
 
 
-;; (->> amended-commits-since-release
-;;   (map
-;;     (fn [commit]
-;;       (into (hash-map )
-;;       (map
-;;         (fn [product] [product (list commit)])
-;;         (git/affected-products commit (rp/get-products))))
-;;        ))
-;;   (apply (partial merge-with into ))
-;;   )
+(def linear-dialog
+  [{:step       :welcome-msg
+    :element    :text
+    :text       (str/join 
+                  "\n" 
+                  ["You are about to create a new product in your repository."
+                  "This assistant will guide you through the steps."])
+    :linebreak? true}
+   {:step     :wait-before-start
+    :element  :wait
+    :seconds  1}
+   {:step     :product-type-question-label
+    :element  :text
+    :text     "Which type of product would you like to add?"}
+   {:step     :select-product-type
+    :element  :choose
+    :options  [{:name "App" :key :app} {:name "Library" :key :library} {:name "Other" :key :other}]}
+   {:step     :use-clci-actions-label
+    :element  :text
+    :text     "Would you like to add aliases to your product to use the following Actions?"}
+   {:step     :select-action-aliases
+    :element  :choose
+    :limit    2
+    :options  [{:name "kondo" :key :kondo} {:name "clj-format" :key :clj-format}]}
+   {:step     :select-template-label
+    :element  :text
+    :text     "Please select a template for your new product:"}
+   {:step     :select-template
+    :element  :choose
+    :filter   (fn [history options] true)
+    :options  [{:name "Clojure" :key :clojure} 
+               {:name "Babashka" :key :babashka}
+               {:name "ClojureScript" :key :clojurescript}
+               {:name "Nbb" :key :nbb}]}])
 
 
 
+(run-linear-dialog linear-dialog)
 
-(update-changelog! amended-commits-since-release)
+(defn create-empty-library
+  ""
+  [root use-kondo? use-cljformat?]
+  )
+
+(defn add-product
+  "Add a new product to the repo."
+  [{:keys [root key kind initial-version no-release?] :or {no-release? false initial-version "0.0.0"}}]
+  ; (if-not (and
+  ;           (s/valid? :clci.repo.product/root root)
+  ;           (s/valid? :clci.repo.product/key key)
+  ;           (s/valid? :clci.repo.product/version initial-version)
+  ;           (s/valid? :clci.repo.product/root root))
+  (let [current-repo  (repo/read-repo)
+        new-product   {:root        root
+                       :key         key
+                       :type        kind
+                       :version     initial-version
+                       :no-release? no-release?}
+        repo'         (update-in current-repo [:products] (fn [old] (conj old new-product)))]
+    (when (= :clojure.spec.alpha/invalid (s/conform :clci.repo/product new-product))
+      (throw (ex-info "The product specification does not conform to spec!" {})))
+    ;(fs/create-dir root)
+    ;(repo/write-repo! repo')
+    )
+  )
+
+(empty? '())
